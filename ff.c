4c07b9d218354211301fba8694682fc194ff2ab6
vibhuti-k <97297327+vibhuti-k@users.noreply.github.com>
2022-09-05 10:50:51 +0530

    Update ff.c

--- ff.c
+++ ff.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int arr1[10], arr2[10], arr3[20], i, j, k=0, p, q ;
+    int arr1[10], arr2[10], arr3[20], i, j, k=0, p, q,x,y ;
     printf("Enter the number of element you want in array 1 : ");
     scanf("%d",&p);
     for(i = 0; i < p; i++)
@@ -17,22 +17,32 @@ int main()
     }
     for(i = 0; i < p; i++)
     {
-        for(j = 0; j < q; j++)
+        arr3[k] = arr1[i];
+        k++;
+    }
+    for(j = 0; j < q; j++)
+    {
+        arr3[k] = arr2[j];
+        k++;
+    }
+   
+   
+    /*for(i = 0; i < p+q; i++)
+    {
+        for(j = 0; j < p+q-i; j++)
         {
-            if(arr1[i] !=arr2[j])
+            if(arr3[j] > arr3[j+1])
             {
-                arr3[k] = arr1[i];
-                k++;
-                arr3[k] = arr2[j];
-                k++;
-            }
-            else
-            {
-                arr3[k] = arr1[i];
+                arr3[j] = x;
+                arr3[j] = arr3[j+1];
+                arr3[j+1] = x;
             }
         }
+    }*/
+    for(i = 0; i < k; k++)
+    {
+        printf("%d, ",arr3[i]);
     }
-    
 
     return 0;
 }
